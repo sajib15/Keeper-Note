892c8cebe33113d2d4a5c50d8026c52cd88fc0ed
sajib15 <60820815+sajib15@users.noreply.github.com>
2022-08-26 11:05:38 +0600

    kepper Notev1

---
+++ build/precache-manifest.446e81f8b3789819e85a581ddbfcfb17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629201c42ffcfe5624cc759c30da24de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ad2cf6984f2abce024",
+    "url": "/static/js/2.79aacc08.chunk.js"
+  },
+  {
+    "revision": "58dde3bc4ddc13b4aa54",
+    "url": "/static/js/main.4dbdfd2f.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
